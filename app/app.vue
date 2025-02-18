5aefd3f8472e9f51d4c050a12bd132f666a89b50
Thomas Taulo <taulo@live.dk>
2025-02-18 13:31:15 +0100

    frontpage setup

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,8 @@
 <template>
-  <div>
+  <div style="display: flex; flex-direction: column;">
+    <img src="public/placeholder.png" style="width: 100%;"/>
+    <div style="align-self: center; margin-top: 5rem">
+      <iframe width="420" height="315" src="https://www.youtube.com/embed/Rj6BhPM1fr8"></iframe>
+    </div>
   </div>
 </template>
