ed35b8e3d5dc9a289a04b0bbf81a12bd92a840c7
Thomas Taulo <taulo@live.dk>
2025-03-02 16:52:18 +0100

    add favicon

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -12,7 +12,15 @@ export default defineNuxtConfig({
     app: {
         head: {
             title: 'Hvordan Er Vejret',
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            link: [
+                { rel: "icon", type: "image/png", href: "/favicon/favicon-96x96.png", sizes: "96x96" },
+                { rel: "icon", type: "image/svg+xml", href: "/favicon/favicon.svg" },
+                { rel:"shortcut icon", href:"/favicon/favicon.ico" },
+                { rel:"apple-touch-icon", sizes:"180x180", href:"/favicon/apple-touch-icon.png" },
+                { rel:"manifest", href:"/favicon/site.webmanifest" },
+            ]
         },
     },
-
 })
