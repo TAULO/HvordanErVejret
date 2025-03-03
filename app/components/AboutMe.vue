c658b368aa235aa7a310184a9f04d52af652c769
Thomas Taulo <taulo@live.dk>
2025-03-03 16:24:35 +0100

    about me component

--- app/components/AboutMe.vue
+++ app/components/AboutMe.vue
@@ -3,9 +3,15 @@
 </script>
 
 <template>
-  
+  <div class="w-full h-[300px] bg-primary flex justify-center flex-col p-10">
+    <h2>
+      I'm Marie.
+    </h2>
+    <p>
+      My hunch is someone you trust mentioned my name, or you stumbled upon one of my videos, quotes or articles online.
+      Whatever path you took, I’m really glad you’re here. This site is full of incredible resources and ideas that can
+      help you change your life (not kidding!). Here’s a quick lay of the land so you can find what you’re looking for and
+      we can start something beautiful together.
+    </p>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
